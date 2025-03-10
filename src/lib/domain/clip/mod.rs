9e011b91c64503bd59c026db7b8d3cc1b64ca898
dezlymacauley <dezlymacauley@proton.me>
2025-03-10 10:27:00 +0200

    Added note on how modules and imports work in Rust

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,27 +1,67 @@
+// SECTION: Listing the sub-directories that are in `src/lib/domain/clip`
+
+// 1. `field` is a sub-directory `src/lib/domain/clip/field`
+// 2. `mod` tells Rust that this is a module. 
+// A module is just a directory that contains Rust files.
+// 3. `pub` means public, which means that any Rust file that is inside
+// src/lib/domain/clip/
+// can use code from the directory src/lib/domain/clip/field 
+
+// E.g. If you had a file like: `src/lib/domain/clip/example_file.rs`
+// and you wanted to use code from: 
+// `src/lib/domain/clip/field/awesome_code.rs` in `example_file.rs`
+
+// Then inside `example_file.rs`, you would add this line:
+// `use super::field::awesome_code`
+
+// `super` is how to import awesome_code.rs in using a relative path.
+// If you wanted to import awesome_code.rs with a relative path then you
+// would have to do this:
+// use crate::lib::domain::clip::field::awesome_code;
+
 pub mod field;
 
-// NOTE: The Clip struct matches the structure of the database
+//_____________________________________________________________________________
+// SECTION: Creating a struct that matches the schema of the SQLite database
 
-/*
 
-    CREATE TABLE IF NOT EXISTS clips
-    (
-        clip_id   TEXT PRIMARY KEY NOT NULL,
-        shortcode TEXT UNIQUE NOT NULL,
-        content   TEXT NOT NULL,
-        title     TEXT,
-        posted    DATETIME NOT NULL,
-        expires   DATETIME,
-        password  TEXT,
-        hits      BIGINT NOT NULL
-    );
 
-*/
+// pub struct Clip {
+//
+// }
 
-// NOTE: Each one of these clips is going to be accessed within the field
-// module that is why there each on has `field::` (path access operator)
+//_____________________________________________________________________________
 
-// The Deserialize and Serialize traits are part of the `serde` crate
+// Import the Deserialize, and Serialize traits from the serde crate
+// use serde::{ Deserialize, Serialize };
+
+// Import `Error` from this error
+// use thiserror::Error;
+
+// #[derive(Debug, Error)]
+// pub enum ClipError {
+//     #[error("invalid password: {0}")]
+//     InvalidPassword(String),
+//
+//     #[error("invalid title: {0}")]
+//     InvalidTitle(String),
+//     
+//     #[error("empty content: {0}")]
+//     EmptyContent,
+//
+//     #[error("invalid date: {0}")]
+//     InvalidDate(String),
+//     
+//     #[error("date parse error: {0}")]
+//     DateParse(#[from] chrono::ParseError),
+//
+//     #[error("id parse error: {0}")]
+//     Id(#[from] uuid::Error),
+//
+//     #[error("hits parse error: {0}")]
+//     Hits(#[from] std::num::TryFromIntError),
+// }
+//
 // #[derive(Debug, Clone, Deserialize, Serialize)]
 // pub struct Clip {
 //
@@ -50,3 +90,22 @@ pub mod field;
 //     pub hits: field::Hits,
 //
 // }
+
+
+// NOTE: The Clip struct matches the structure of the database
+
+/*
+
+    CREATE TABLE IF NOT EXISTS clips
+    (
+        clip_id   TEXT PRIMARY KEY NOT NULL,
+        shortcode TEXT UNIQUE NOT NULL,
+        content   TEXT NOT NULL,
+        title     TEXT,
+        posted    DATETIME NOT NULL,
+        expires   DATETIME,
+        password  TEXT,
+        hits      BIGINT NOT NULL
+    );
+
+*/
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,16 +1,71 @@
-// NOTE: This is a list of directories 
-// that are inside the dirctory `src/lib/`
-
-// This is like a table of contents for the library of the project
-// 1. `mod` means module, which is simply a directory that contains Rust code
-// 2. `pub mod` means that the directories listed 
-// here are public rather than private (which is the default in Rust).
+//_____________________________________________________________________________
+// SECTION: Listing the sub-directories that are in `src/lib/domain/`
 
 pub mod data;
 pub mod domain;
 pub mod service;
 pub mod web;
 
-// Sometimes this mod.rs file is called, `lib.rs`
+// ABOUT: How modules and file imports work in Rust
+/*
 
-//_____________________________________________________________________________
+    NOTE: The sructure
+    -------------------
+    src/
+    ├── lib/
+    │   ├── data/
+    │   │   ├── weapons_collected.rs
+    │   │   ├── mod.rs
+    │   ├── player_stats.rs
+    │   ├── mod.rs
+
+
+    NOTE: Modules
+
+    `data` is a sub-directory located at `src/lib/data`
+
+    `mod` tells Rust that the `data` directory is a module. 
+    A module is just a directory that contains some Rust files,
+    or a single .rs file.
+    
+    To avoid confusion, Rust will not allow you to have a file that has
+    the same name as a sub directory.
+    
+    E.g. Rust will not compile if you try to do this:
+
+    src/
+    ├── lib/
+    │   ├── data/
+            | -- weapons_collected/
+    │   │   ├── weapons_collected.rs
+    │   │   ├── mod.rs
+    │   ├── player_stats.rs
+    │   ├── mod.rs
+
+    In Rust spaces between words in a module name 
+    must be separted by underscores. Hyphens are not allowed.
+
+    `pub mod` means public module.
+    This means that any Rust file that at the location: `src/lib`,
+    can use the code from any file that is located in `src/lib/data` 
+
+    ----------------------------------------------------------------
+    NOTE: Imports
+
+    E.g. If you had a file called `player_stats.rs`,
+    that was located at `src/lib/player_stats.rs`
+
+    You want to use code from a file called `weapons_collected.rs`,
+    that is located at `src/lib/data/weapons_collected.rs`
+
+    // Then inside `player_stats.rs`, you would add this line:
+    // `use super::data::weapons_collected`
+    // `super` is how to import weapons_collected.rs in using a relative path.
+    // `super` in this case means "start from src/lib/"
+
+    // If you wanted to import weapons_collected.rs 
+    // with a relative path then you would have to do this:
+    // use crate::lib::data::weapons_collected;
+    // `crate` always means start from `src/`
+
+*/
